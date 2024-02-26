56a6ab406ac73706b6a8ce5e72f96baf8ccf2f8a
jmaczan <jedrzejpawel@maczan.pl>
2024-02-26 17:36:43 +0100

    feat: update code

---
+++ lib/index.d.ts
@@ -0,0 +1,13 @@
+import OpenAI from 'openai';
+export declare function poll(
+  openai: OpenAI,
+  thread: OpenAI.Beta.Threads.Thread,
+  run: OpenAI.Beta.Threads.Runs.Run,
+  options?: PollOptions
+): Promise<OpenAI.Beta.Threads.Runs.Run>;
+export interface PollOptions {
+  maxAttempts: number;
+  intervalInMilliseconds: number;
+  showLogs: boolean;
+}
+export declare const defaultPollOptions: PollOptions;
