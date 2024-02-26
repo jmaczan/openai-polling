4c10118a55674ae7eb940f228e1d034ef310b808
jmaczan <jedrzejpawel@maczan.pl>
2024-02-26 17:50:55 +0100

    feat: update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-export declare function poll(openai: OpenAI, thread: OpenAI.Beta.Threads.Thread, run: OpenAI.Beta.Threads.Runs.Run, options: Partial<PollOptions>): Promise<OpenAI.Beta.Threads.Runs.Run>;
+export declare function poll(openai: OpenAI, thread: OpenAI.Beta.Threads.Thread, run: OpenAI.Beta.Threads.Runs.Run, options?: Partial<PollOptions>): Promise<OpenAI.Beta.Threads.Runs.Run>;
 export interface PollOptions {
     maxAttempts: number;
     intervalInMilliseconds: number;
